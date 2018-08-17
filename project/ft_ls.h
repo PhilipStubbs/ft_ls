3785a63b2e81c6e1f7ce04ac89d664bc6158b895
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-08-17 13:25:30 +0200

    base setup

--- project/ft_ls.h
+++ project/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: pstubbs <pstubbs@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/17 10:31:00 by pstubbs           #+#    #+#             */
-/*   Updated: 2018/08/17 10:32:38 by pstubbs          ###   ########.fr       */
+/*   Updated: 2018/08/17 12:47:06 by pstubbs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,32 @@
 # define FT_LS_H
 
 #include <sys/types.h>
+#include <sys/stat.h>
+#include "libft/includes/ft_printf.h"
 
-typedef	struct		s_list
+typedef	struct		s_info
 {
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
-}					t_list;
+	char			*permissions;
+	char			*name;
+	char			*links;
+	char			*owner;
+	char			*group;
+	char			*size;
+	char			*date;
+	char			*type;
+	struct	stat	*info;
+	struct	s_info	*next;
+}					t_info;
+
+
+typedef	struct		s_ls
+{
+	t_info			*hold;
+	int				l;
+	int				R;
+	int				a;
+	int				r;
+	int				t;
+}					t_ls;
 
 #endif
